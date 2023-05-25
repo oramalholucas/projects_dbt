fd80840c113f4dd3e049c43ca2df84ce735b44bd
oramalholucas@gmail.com <oramalholucas@gmail.com>
2023-05-25 01:08:03 +0000

    Criacao de documentacao dos modelos, realizacao de testes e configurando materializacoes

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -9,8 +9,14 @@ with
         from {{ ref('stg_orders')}}
     )
 
+    , order_details as (
+        select *
+        from {{ ref('stg_order_details')}}
+    )
+
     select
         customers.sk_customer
         , orders.*
     from orders
     left join customers on orders.customer_id = customers.customer_id
+    left join order_details on orders.order_id = order_details.order_id
--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,7 +1,15 @@
+{{ config(materialized='incremental') }}
+
 with
     orders as (
         select *
         from {{source('northwind', 'orders')}}
+        {% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  where order_date > (select max(order_date) from {{ this }})
+
+        {% endif %}
     )
 
 select *
